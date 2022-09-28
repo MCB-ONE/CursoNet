ecb0d2a7f24b9484e48532aa1572e2f50f109652
toni <toni.one.dev@gmail.com>
2022-09-28 12:39:52 +0200

    Logg_implementado_en_contexto

--- BussinesLogic/Data/UniversityDBContext.cs
+++ BussinesLogic/Data/UniversityDBContext.cs
@@ -1,14 +1,17 @@
 ﻿using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 using BussinesLogic.Seeder;
+using Microsoft.Extensions.Logging;
 
 namespace BussinesLogic.Data
 {
     public class UniversityDBContext : DbContext
     {
-        public UniversityDBContext(DbContextOptions<UniversityDBContext> options) : base(options)
-        {
+        private readonly ILoggerFactory _loggerFactory;
 
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options, ILoggerFactory loggerFactory) : base(options)
+        {
+            _loggerFactory = loggerFactory;
         }
 
         //Añadir DBSets (las tablas de la base de datos)
@@ -17,6 +20,23 @@ namespace BussinesLogic.Data
         public DbSet<Category>? Categories { get; set; }
         public DbSet<Student>? Students { get; set; }
         public DbSet<Core.Entities.Index>? Indexes { get; set; }
+
+        // Configurar logger en el contexto
+        // Permite generar loggs cuando se interactue con el contexto (operaciones CRUD)
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var logger = _loggerFactory.CreateLogger<UniversityDBContext>();
+
+            // Especificamos el nivel de logs y donde guardarlos
+            //optionsBuilder.LogTo(d => logger.Log(LogLevel.Information, d, new[] { DbLoggerCategory.Database.Name })); // Con esta config se egistrarían todas las interacciones con la base de datos (demasiada información)
+            //optionsBuilder.EnableSensitiveDataLogging(); // Habilitar registrar datos "sensibles" en los mensajes de log
+
+            // Personalizamos y seleccionamos solo aquellas operaciones que queremos registrar
+            // Filtramos los logs que seran de nivel informativo y si hay error que sea lo mas detallado posible
+            optionsBuilder.LogTo(d => logger.Log(LogLevel.Information, d, new[] { DbLoggerCategory.Database.Name }), LogLevel.Information)
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
